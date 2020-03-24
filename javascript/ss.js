1819b1d3739e0e9079aed9fa244107946c749ac7
slncspc <62605819+slncspc@users.noreply.github.com>
2020-03-24 20:29:06 +0600

    Update ss.js

--- javascript/ss.js
+++ javascript/ss.js
@@ -1,20 +1,15 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.set(-12, 10, -12);
+camera.position.set(-12, 5, -12);
 camera.rotation.set(-1, 0, 0);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
 var flat = new THREE.Group();
 for(var i = 0; i < 24; i++) {
-	for(var j = 9; j < 24; j++) {
+	for(var j = 0; j < 24; j++) {
 		var land = new THREE.Mesh( new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 0x3c913d}));
 		land.position.set(-1*i, 0, -1*j);
 		flat.add(land);
@@ -22,10 +17,28 @@ for(var i = 0; i < 24; i++) {
 }
 scene.add(flat);
 
+var duck = new THREE.Group();
+for(var i = 0; i < 4; i++) {
+	switch(i) {
+		case 0:
+			var hor = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color : 0xeba9e6}));
+			hor.position.set(-9, 1, -21);
+			break;
+		case 1:
+			var hor = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color : 0xeba9e6}));
+			hor.position.set(-10, 1, -21);
+			break;
+		case 2:
+			var hor = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color : 0xeba9e6}));
+			hor.position.set(-11, 1, -21);
+			break;
+	}
+}
+
+scene.add(duck);
+
 var animate = function () {
 	requestAnimationFrame( animate );
-
-	
 
 	renderer.render( scene, camera );
 };
