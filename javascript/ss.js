04c35333ec0bfe226282c4afc0fa7585f2dd59cd
slncspc <62605819+slncspc@users.noreply.github.com>
2020-03-24 20:19:52 +0600

    Update ss.js

--- javascript/ss.js
+++ javascript/ss.js
@@ -1,6 +1,6 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.set(-12, 5, -12);
+camera.position.set(-12, 10, -12);
 camera.rotation.set(-1, 0, 0);
 
 var renderer = new THREE.WebGLRenderer();
