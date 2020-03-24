1fb4d429310010217fffdf2ee67d09203ca383ff
slncspc <62605819+slncspc@users.noreply.github.com>
2020-03-24 20:18:45 +0600

    Update ss.js

--- javascript/ss.js
+++ javascript/ss.js
@@ -1,24 +1,33 @@
-			var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+camera.position.set(-12, 5, -12);
+camera.rotation.set(-1, 0, 0);
 
-			var renderer = new THREE.WebGLRenderer();
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			document.body.appendChild( renderer.domElement );
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-			var geometry = new THREE.BoxGeometry();
-			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			var cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-			camera.position.z = 5;
+var flat = new THREE.Group();
+for(var i = 0; i < 24; i++) {
+	for(var j = 9; j < 24; j++) {
+		var land = new THREE.Mesh( new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 0x3c913d}));
+		land.position.set(-1*i, 0, -1*j);
+		flat.add(land);
+	}
+}
+scene.add(flat);
 
-			var animate = function () {
-				requestAnimationFrame( animate );
+var animate = function () {
+	requestAnimationFrame( animate );
 
-				cube.rotation.x += 0.01;
-				cube.rotation.y += 0.01;
+	
 
-				renderer.render( scene, camera );
-			};
+	renderer.render( scene, camera );
+};
 
-			animate();
+animate();
